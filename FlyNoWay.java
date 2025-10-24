e65a922b9d379784bb861e2bb19690506454290a
Akanksha <akankshazc23@icloud.com>
2025-10-24 07:29:56 +0530

    Add comments to FlyNoWay and FlyWithWings classes for clarity

--- FlyNoWay.java
+++ FlyNoWay.java
@@ -1,3 +1,6 @@
+// Implement the FlyBehavior interface 
+// in concrete class for ducks that can't fly
+
 public class FlyNoWay implements FlyBehavior {
     public void fly() {
         System.out.println("I can't fly.");
--- FlyWithWings.java
+++ FlyWithWings.java
@@ -1,3 +1,6 @@
+// Implement the FlyBehavior interface 
+// in concrete class for ducks that fly with wings
+
 public class FlyWithWings implements FlyBehavior {
     public void fly() {
         System.out.println("I'm flying!");

c27e1e19bf69b72cd2731781d46a33065d54e6f5
Akanksha <akankshazc23@icloud.com>
2025-10-24 07:29:48 +0530

    Add FlyRocketPowered class implementing FlyBehavior for rocket-powered flight

---
+++ FlyRocketPowered.java
@@ -0,0 +1,8 @@
+// Implement the FlyBehavior interface 
+// in concrete class for ducks that fly with a rocket
+
+public class FlyRocketPowered implements FlyBehavior {
+    public void fly() {
+        System.out.println("I'm flying with a rocket!");
+    }
+}

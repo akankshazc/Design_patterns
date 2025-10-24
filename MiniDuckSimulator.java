ae3f90ac4e6282553bc9db7fc5bbe13583d3374c
Akanksha <akankshazc23@icloud.com>
2025-10-24 07:28:50 +0530

    Add ModelDuck instance and demonstrate using setter method to change Duck behavior at runtime

--- MiniDuckSimulator.java
+++ MiniDuckSimulator.java
@@ -1,7 +1,20 @@
+// MiniDuckSimulator to create instances of Duck 
+// subclasses with different behaviors
+
 public class MiniDuckSimulator {
     public static void main(String[] args) {
+
+        // MallardDuck class
         Duck mallard = new MallardDuck();
         mallard.performQuack();
         mallard.performFly();
+
+        // ModelDuck class
+        Duck model = new ModelDuck();
+        model.performFly();
+
+        // Changing fly behavior at runtime using the setter method
+        model.setFlyBehavior(new FlyRocketPowered());
+        model.performFly();
     }
 }

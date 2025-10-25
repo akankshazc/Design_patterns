5e709398a8ec1a62e195551c4fa06bd484f3bea5
Akanksha <akankshazc23@icloud.com>
2025-10-25 22:31:56 +0530

    Add CurrentConditionDisplay class to implement Observer and Display interfaces for weather data

---
+++ hf_2/CurrentConditionDisplay.java
@@ -0,0 +1,22 @@
+package hf_2;
+
+public class CurrentConditionDisplay implements Display, Observer {
+    private float temp;
+    private float humidity;
+    private WeatherData weatherData;
+
+    public CurrentConditionDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    public void update(float temp, float humidity, float pressure) {
+        this.temp = temp;
+        this.humidity = humidity;
+        display();
+    }
+
+    public void display() {
+        System.out.println("Current conditions: " + temp + "F degrees and " + humidity + "% humidity");
+    }
+}

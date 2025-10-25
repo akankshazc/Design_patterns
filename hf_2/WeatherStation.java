661eee9e39d224cb42a340a321c28208be7ff08f
Akanksha <akankshazc23@icloud.com>
2025-10-25 22:38:26 +0530

    Add WeatherStation class to initialize weather data and display observers

---
+++ hf_2/WeatherStation.java
@@ -0,0 +1,18 @@
+package hf_2;
+
+public class WeatherStation {
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionDisplay currentDisplay = new CurrentConditionDisplay(weatherData);
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+
+        weatherData.setMeasurements(80, 65, 304f);
+        weatherData.setMeasurements(82, 70, 29.2f);
+        weatherData.setMeasurements(78, 90, 29.2f);
+
+        weatherData.removeObserver(forecastDisplay);
+        weatherData.setMeasurements(62, 90, 28.1f);
+    }
+}

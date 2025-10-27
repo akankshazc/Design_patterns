bd652778cfd717f7eb4708e509c5eb42322d0cc1
Akanksha <akankshazc23@icloud.com>
2025-10-27 21:32:27 +0530

    Add Beverage abstract class to define beverage description and cost method

---
+++ hf_3/Starbuzz/Beverage.java
@@ -0,0 +1,11 @@
+package hf_3.Starbuzz;
+
+public abstract class Beverage {
+    String description = "Unknown Beverage";
+
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract double cost();
+}

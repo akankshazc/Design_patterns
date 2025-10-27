49f50f30d2fa8e92c10da97d630f4024d1606692
Akanksha <akankshazc23@icloud.com>
2025-10-27 21:36:29 +0530

    Add Whip class to implement CondimentDecorator for beverage cost and description

---
+++ hf_3/Starbuzz/Whip.java
@@ -0,0 +1,15 @@
+package hf_3.Starbuzz;
+
+public class Whip extends CondimentDecorator {
+    public Whip(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    public String getDescription() {
+        return beverage.getDescription() + ", Whip";
+    }
+
+    public double cost() {
+        return .10 + beverage.cost();
+    }
+}

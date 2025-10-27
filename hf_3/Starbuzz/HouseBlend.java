89154d6fb99ed1284bda928a1437c918b193a278
Akanksha <akankshazc23@icloud.com>
2025-10-27 21:35:10 +0530

    Add HouseBlend class to implement Beverage with specific description and cost

---
+++ hf_3/Starbuzz/HouseBlend.java
@@ -0,0 +1,11 @@
+package hf_3.Starbuzz;
+
+public class HouseBlend extends Beverage {
+    public HouseBlend() {
+        description = "House Blend Coffee";
+    }
+
+    public double cost() {
+        return .89;
+    }
+}

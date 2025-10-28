dabea0b1b15a35d142b313c98ccecb6c52eb5b81
Akanksha <akankshazc23@icloud.com>
2025-10-28 23:18:51 +0530

    Add VeggiePizza class to define veggie pizza properties and toppings

---
+++ hf_4/pizza/VeggiePizza.java
@@ -0,0 +1,16 @@
+package hf_4.pizza;
+
+public class VeggiePizza extends Pizza {
+    public VeggiePizza() {
+        name = "Veggie Pizza";
+        dough = "Crust";
+        sauce = "Marinara sauce";
+
+        toppings.add("Shredded mozzarella");
+        toppings.add("Grated Parmesan");
+        toppings.add("Diced onion");
+        toppings.add("Sliced mushrooms");
+        toppings.add("Sliced red pepper");
+        toppings.add("Sliced black olives");
+    }
+}

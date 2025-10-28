fdb42431349b1f4213157e02e589311d4872da90
Akanksha <akankshazc23@icloud.com>
2025-10-28 23:18:36 +0530

    Add CheesePizza class to define cheese pizza properties and toppings

---
+++ hf_4/pizza/CheesePizza.java
@@ -0,0 +1,12 @@
+package hf_4.pizza;
+
+public class CheesePizza extends Pizza {
+    public CheesePizza() {
+        name = "Cheese Pizza";
+        dough = "Regular Crust";
+        sauce = "Marinara Pizza Sauce";
+
+        toppings.add("Fresh Mozzarella");
+        toppings.add("Parmesan");
+    }
+}

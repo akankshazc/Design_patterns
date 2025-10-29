3559708c8288ac62576e40643c4e73c01908737f
Akanksha <akankshazc23@icloud.com>
2025-10-29 21:42:04 +0530

    Add ChicagoStyleCheesePizza class to define Chicago-style cheese pizza properties and behavior

---
+++ hf_4/pizzaf/ChicagoStyleCheesePizza.java
@@ -0,0 +1,16 @@
+package hf_4.pizzaf;
+
+public class ChicagoStyleCheesePizza extends Pizza {
+
+    public ChicagoStyleCheesePizza() {
+        name = "Chicago Style Deep Dish Cheese Pizza";
+        dough = "Extra Thick Crust Dough";
+        sauce = "Plum Tomato Sauce";
+
+        toppings.add("Shredded Mozzarella Cheese");
+    }
+
+    void cut() {
+        System.out.println("Cutting the pizza into square slices");
+    }
+}

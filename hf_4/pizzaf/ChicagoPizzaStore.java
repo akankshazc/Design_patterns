783f07a7dacae3fc5a54a1eb90fef3344c594632
Akanksha <akankshazc23@icloud.com>
2025-10-29 21:41:56 +0530

    Add ChicagoPizzaStore class to implement pizza creation for Chicago-style pizzas

---
+++ hf_4/pizzaf/ChicagoPizzaStore.java
@@ -0,0 +1,17 @@
+package hf_4.pizzaf;
+
+public class ChicagoPizzaStore extends PizzaStore {
+
+    Pizza createPizza(String item) {
+        if (item.equals("cheese")) {
+            return new ChicagoStyleCheesePizza();
+        } else if (item.equals("veggie")) {
+            return new ChicagoStyleVeggiePizza();
+        } else if (item.equals("clam")) {
+            return new ChicagoStyleClamPizza();
+        } else if (item.equals("pepperoni")) {
+            return new ChicagoStylePepperoniPizza();
+        } else
+            return null;
+    }
+}

855db4d09b9777989d8eb809982efd799591abe1
Akanksha <akankshazc23@icloud.com>
2025-10-31 17:39:20 +0530

    Add Cook class to handle burger and fries preparation

---
+++ hf_6/diner/Cook.java
@@ -0,0 +1,15 @@
+package hf_6.diner;
+
+public class Cook {
+
+    public Cook() {
+    };
+
+    public void makeBurger() {
+        System.out.println("Making a burger");
+    }
+
+    public void makeFries() {
+        System.out.println("Making fries");
+    }
+}

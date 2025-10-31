de27aa6100dbee51d0599ae2425b0af4bca7fdcd
Akanksha <akankshazc23@icloud.com>
2025-10-31 17:38:56 +0530

    Add Waitress class to handle order taking in the diner system

---
+++ hf_6/diner/Waitress.java
@@ -0,0 +1,13 @@
+package hf_6.diner;
+
+public class Waitress {
+    Order order;
+
+    public Waitress() {
+    }
+
+    public void takeOrder(Order order) {
+        this.order = order;
+        order.orderUp();
+    }
+}

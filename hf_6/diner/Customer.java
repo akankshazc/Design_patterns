0a5b8077fbe2e32ffa9f7a5c52e2b1f2c81b355d
Akanksha <akankshazc23@icloud.com>
2025-10-31 17:39:26 +0530

    Add Customer class to manage order creation and interaction with Waitress

---
+++ hf_6/diner/Customer.java
@@ -0,0 +1,18 @@
+package hf_6.diner;
+
+public class Customer {
+    Waitress waitress;
+    Order order;
+
+    public Customer(Waitress waitress) {
+        this.waitress = waitress;
+    }
+
+    public void createOrder(Order order) {
+        this.order = order;
+    }
+
+    public void hungry() {
+        waitress.takeOrder(order);
+    }
+}

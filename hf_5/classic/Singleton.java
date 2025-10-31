dce021a890e50eb3f62fd8e1be6f4dc660b80bd3
Akanksha <akankshazc23@icloud.com>
2025-10-31 17:38:09 +0530

    Add Singleton and SingletonClient classes to implement and demonstrate the Singleton pattern

---
+++ hf_5/classic/Singleton.java
@@ -0,0 +1,22 @@
+package hf_5.classic;
+
+// NOT Thread safe
+
+public class Singleton {
+    private static Singleton uniqueInstance;
+
+    private Singleton() {
+    };
+
+    public static Singleton getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }
+
+    // other methods here
+    public String getDescription() {
+        return "I'm a classic Singleton!";
+    }
+}
---
+++ hf_5/classic/SingletonClient.java
@@ -0,0 +1,8 @@
+package hf_5.classic;
+
+public class SingletonClient {
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+        System.out.println(singleton.getDescription());
+    }
+}

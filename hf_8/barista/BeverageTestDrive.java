aecd84659d3e5f23ac671abb4efdb1ed4cfc2280
Akanksha <akankshazc23@icloud.com>
2025-11-01 21:17:36 +0530

    Add BeverageTestDrive class to demonstrate beverage preparation with Tea and Coffee

---
+++ hf_8/barista/BeverageTestDrive.java
@@ -0,0 +1,23 @@
+package hf_8.barista;
+
+public class BeverageTestDrive {
+    public static void main(String[] args) {
+        Tea tea = new Tea();
+        Coffee coffee = new Coffee();
+
+        System.out.println("\nMaking tea...");
+        tea.prepareRecipe();
+
+        System.out.println("\nMaking Coffee...");
+        coffee.prepareRecipe();
+
+        TeaWithHook teaHook = new TeaWithHook();
+        CoffeeWithHook coffeeHook = new CoffeeWithHook();
+
+        System.out.println("\nMaking tea...");
+        teaHook.prepareRecipe();
+
+        System.out.println("\nMaking coffee...");
+        coffeeHook.prepareRecipe();
+    }
+}

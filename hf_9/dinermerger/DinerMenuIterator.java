84db80f3403da394fbb9d67f0b4cf4fbf63ff085
Akanksha <akankshazc23@icloud.com>
2025-11-02 10:25:16 +0530

    Add DinerMenuIterator class to implement iterator functionality for DinerMenu

---
+++ hf_9/dinermerger/DinerMenuIterator.java
@@ -0,0 +1,20 @@
+package hf_9.dinermerger;
+
+public class DinerMenuIterator implements Iterator {
+    MenuItem[] items;
+    int position = 0;
+
+    public DinerMenuIterator(MenuItem[] items) {
+        this.items = items;
+    }
+
+    public MenuItem next() {
+        MenuItem menuItem = items[position];
+        position = position + 1;
+        return menuItem;
+    }
+
+    public boolean hasNext() {
+        return items.length > position;
+    }
+}

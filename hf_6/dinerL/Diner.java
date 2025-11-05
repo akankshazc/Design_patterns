029e242d723ec441ecb8afc53e23b139c952a1f7
Akanksha <akankshazc23@icloud.com>
2025-11-05 21:43:37 +0530

    Add Diner class to initialize and demonstrate interaction between Cook, Waitress, and Customer

---
+++ hf_6/dinerL/Diner.java
@@ -0,0 +1,12 @@
+package hf_6.dinerL;
+
+public class Diner {
+    public static void main(String[] args) {
+        Cook cook = new Cook();
+        Waitress waitress = new Waitress();
+        Customer customer = new Customer(waitress, cook);
+
+        customer.createOrder();
+        customer.hungry();
+    }
+}

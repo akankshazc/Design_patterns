c7dc26f0cd4f9543dffd1a782449c64c36d77d45
Akanksha <akankshazc23@icloud.com>
2025-11-05 21:44:42 +0530

    Add CeilingFan, CeilingFanOnCommand, and CeilingFanOffCommand classes for fan control functionality

---
+++ hf_6/remote/CeilingFanOffCommand.java
@@ -0,0 +1,13 @@
+package hf_6.remote;
+
+public class CeilingFanOffCommand implements Command {
+    CeilingFan ceilingFan;
+
+    public CeilingFanOffCommand(CeilingFan ceilingFan) {
+        this.ceilingFan = ceilingFan;
+    }
+
+    public void execute() {
+        ceilingFan.off();
+    }
+}

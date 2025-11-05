51a9e6d3f356ba5688635331706813ae0b99607d
Akanksha <akankshazc23@icloud.com>
2025-11-05 21:42:41 +0530

    Add class Cook for diner

---
+++ hf_6/dinerL/Cook.java
@@ -0,0 +1,14 @@
+package hf_6.dinerL;
+
+public class Cook {
+    public Cook() {
+    }
+
+    public void makeBurger() {
+        System.out.println("Making a burger");
+    }
+
+    public void makeFries() {
+        System.out.println("Making fries");
+    }
+}

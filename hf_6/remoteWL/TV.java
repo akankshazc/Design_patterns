fe04d373a20b2b065a6ab9b0d305ef38d5f2836a
Akanksha <akankshazc23@icloud.com>
2025-11-06 10:27:17 +0530

    Add TV class for television control functionality

---
+++ hf_6/remoteWL/TV.java
@@ -0,0 +1,23 @@
+package hf_6.remoteWL;
+
+public class TV {
+    String location;
+    int channel;
+
+    public TV(String location) {
+        this.location = location;
+    }
+
+    public void on() {
+        System.out.println(location + " TV is on");
+    }
+
+    public void off() {
+        System.out.println(location + " TV is off");
+    }
+
+    public void setInputChannel() {
+        this.channel = 3;
+        System.out.println(location + " TV input channel set to VCR");
+    }
+}

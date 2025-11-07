811c0834e5aa5219b9e595eb4b3b30ca2fc25519
Akanksha <akankshazc23@icloud.com>
2025-11-07 22:17:42 +0530

    Add Light class for controlling light functionality

---
+++ hf_6/simpleremote/Light.java
@@ -0,0 +1,14 @@
+package hf_6.simpleremote;
+
+public class Light {
+    public Light() {
+    }
+
+    public void on() {
+        System.out.println("Light is on");
+    }
+
+    public void off() {
+        System.out.println("Light is off");
+    }
+}

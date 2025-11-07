b24ef5c1fbd1688be72e1275cf88974f1deb18cd
Akanksha <akankshazc23@icloud.com>
2025-11-07 22:18:51 +0530

    Add GarageDoor class for controlling garage door and light functionality

---
+++ hf_6/simpleremoteWL/GarageDoor.java
@@ -0,0 +1,26 @@
+package hf_6.simpleremoteWL;
+
+public class GarageDoor {
+    public GarageDoor() {
+    }
+
+    public void up() {
+        System.out.println("Garage Door is open");
+    }
+
+    public void down() {
+        System.out.println("Garage Door is closed");
+    }
+
+    public void stop() {
+        System.out.println("Garage Door is Stopped");
+    }
+
+    public void lightOn() {
+        System.out.println("Garage light is on");
+    }
+
+    public void lightOff() {
+        System.out.println("Garage light is off");
+    }
+}

cd4377f4c5499aa77221759b8f5b8163cd8d709f
Akanksha <akankshazc23@icloud.com>
2025-11-07 22:19:15 +0530

    Add SimpleRemoteControl class to serve as the invoker for command execution

---
+++ hf_6/simpleremoteWL/SimpleRemoteControl.java
@@ -0,0 +1,17 @@
+package hf_6.simpleremoteWL;
+
+// this is the invoker
+public class SimpleRemoteControl {
+    Command slot;
+
+    public SimpleRemoteControl() {
+    }
+
+    public void setCommand(Command command) {
+        slot = command;
+    }
+
+    public void buttonWasPressed() {
+        slot.execute();
+    }
+}

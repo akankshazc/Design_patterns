fea304b9b98fdbddfed2262a63e25c92cebbdd71
Akanksha <akankshazc23@icloud.com>
2025-11-09 20:18:46 +0530

    Add Beverage and coffee classes to implement beverage structure

---
+++ hf_3/starbuzzwsizes/DarkRoast.java
@@ -0,0 +1,11 @@
+package hf_3.starbuzzwsizes;
+
+public class DarkRoast extends Beverage {
+    public DarkRoast() {
+        description = "Dark Roast Coffee";
+    }
+
+    public double cost() {
+        return 0.99;
+    }
+}
---
+++ hf_3/starbuzzwsizes/Espresso.java
@@ -0,0 +1,11 @@
+package hf_3.starbuzzwsizes;
+
+public class Espresso extends Beverage {
+    public Espresso() {
+        description = "Espresso";
+    }
+
+    public double cost() {
+        return 1.99;
+    }
+}

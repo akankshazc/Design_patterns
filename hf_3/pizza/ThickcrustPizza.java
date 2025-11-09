15b096fc08bf9e7d942bb572325e81a87e4f12ac
Akanksha <akankshazc23@icloud.com>
2025-11-09 20:17:13 +0530

    Add Pizza, ThickcrustPizza, and ThincrustPizza classes to implement pizza structure

---
+++ hf_3/pizza/ThickcrustPizza.java
@@ -0,0 +1,12 @@
+package hf_3.pizza;
+
+public class ThickcrustPizza extends Pizza {
+
+    public ThickcrustPizza() {
+        description = "Thick crust pizza, with tomato sauce";
+    }
+
+    public double cost() {
+        return 7.99;
+    }
+}
---
+++ hf_3/pizza/ThincrustPizza.java
@@ -0,0 +1,12 @@
+package hf_3.pizza;
+
+public class ThincrustPizza extends Pizza {
+
+    public ThincrustPizza() {
+        description = "Thin crust pizza, with tomato sauce";
+    }
+
+    public double cost() {
+        return 7.99;
+    }
+}

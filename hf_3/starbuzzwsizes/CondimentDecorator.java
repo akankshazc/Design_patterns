1e5813fb0458b10fe074ac48cd8108b095798c0f
Akanksha <akankshazc23@icloud.com>
2025-11-09 20:18:57 +0530

    Add CondimentDecorator and concrete condiment classes (Milk, Soy, Whip) to enhance beverage functionality

---
+++ hf_3/starbuzzwsizes/CondimentDecorator.java
@@ -0,0 +1,11 @@
+package hf_3.starbuzzwsizes;
+
+public abstract class CondimentDecorator extends Beverage {
+    public Beverage beverage;
+
+    public abstract String getDescription();
+
+    public Size getSize() {
+        return beverage.getSize();
+    }
+}

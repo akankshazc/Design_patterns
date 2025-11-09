a27452ec503966984b5c5fd208a91cc8c8213c50
Akanksha <akankshazc23@icloud.com>
2025-11-09 20:16:50 +0530

    Add Example class to demonstrate observer pattern functionality

---
+++ hf_2/simple/Example.java
@@ -0,0 +1,14 @@
+package hf_2.simple;
+
+public class Example {
+
+    public static void main(String[] args) {
+        SimpleSubject simpleSubject = new SimpleSubject();
+
+        SimpleObserver simpleObserver = new SimpleObserver(simpleSubject);
+
+        simpleSubject.setValue(80);
+
+        simpleSubject.removeObserver(simpleObserver);
+    }
+}

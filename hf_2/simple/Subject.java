93741d750e74db49557b332e706cc42690a72501
Akanksha <akankshazc23@icloud.com>
2025-11-09 20:16:36 +0530

    Add SimpleSubject and Subject interface to implement observer pattern

---
+++ hf_2/simple/Subject.java
@@ -0,0 +1,9 @@
+package hf_2.simple;
+
+public interface Subject {
+    public void registerObserver(Observer o);
+
+    public void removeObserver(Observer o);
+
+    public void notifyObservers();
+}

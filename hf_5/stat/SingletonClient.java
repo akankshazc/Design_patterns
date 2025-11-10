de6aa94a6bfcee86d4af3b6cb256e7e61b13c16a
Akanksha <akankshazc23@icloud.com>
2025-11-10 23:39:18 +0530

    Add Singleton and SingletonClient classes to implement Singleton design pattern

---
+++ hf_5/stat/SingletonClient.java
@@ -0,0 +1,8 @@
+package hf_5.stat;
+
+public class SingletonClient {
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+        System.out.println(singleton.getDescription());
+    }
+}

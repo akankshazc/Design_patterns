8446d3dff6256c82554da48139c6e10b31746c55
Akanksha <akankshazc23@icloud.com>
2025-11-10 23:38:43 +0530

    Add CalendarTestDrive class to demonstrate usage of PacificCalendar and ZoneFactory

---
+++ hf_4/challenge/CalendarTestDrive.java
@@ -0,0 +1,16 @@
+package hf_4.challenge;
+
+import java.util.*;
+
+public class CalendarTestDrive {
+
+    public static void main(String[] args) {
+        ZoneFactory zoneFactory = new ZoneFactory();
+        Calendar calendar = new PacificCalendar(zoneFactory);
+
+        List<String> appts = Arrays.asList("appt 1", "appt 2");
+
+        calendar.createCalendar(appts);
+        calendar.print();
+    }
+}

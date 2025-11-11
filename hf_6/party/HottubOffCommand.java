c67fb85617ac2399e56589206a1f161e061c6fe4
Akanksha <akankshazc23@icloud.com>
2025-11-11 09:03:01 +0530

    Add Hottub and command classes to implement hottub control with command pattern

---
+++ hf_6/party/HottubOffCommand.java
@@ -0,0 +1,18 @@
+package hf_6.party;
+
+public class HottubOffCommand implements Command {
+    Hottub hottub;
+
+    public HottubOffCommand(Hottub hottub) {
+        this.hottub = hottub;
+    }
+
+    public void execute() {
+        hottub.setTemperature(98);
+        hottub.off();
+    }
+
+    public void undo() {
+        hottub.on();
+    }
+}

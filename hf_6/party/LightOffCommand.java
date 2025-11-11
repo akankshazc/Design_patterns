1d03fb1395e3e89daaf0543ba751f0084919f647
Akanksha <akankshazc23@icloud.com>
2025-11-11 09:03:10 +0530

    Add Light, LightOnCommand, and LightOffCommand classes to implement light control with command pattern

---
+++ hf_6/party/LightOffCommand.java
@@ -0,0 +1,17 @@
+package hf_6.party;
+
+public class LightOffCommand implements Command {
+    Light light;
+
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    public void execute() {
+        light.off();
+    }
+
+    public void undo() {
+        light.on();
+    }
+}

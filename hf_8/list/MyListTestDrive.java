e36a3f673067d3e828859f30868d3f2e98c8dfec
Akanksha <akankshazc23@icloud.com>
2025-11-17 23:44:38 +0530

    Add MyStringList and MyListTestDrive classes to implement and demonstrate custom list functionality

---
+++ hf_8/list/MyListTestDrive.java
@@ -0,0 +1,29 @@
+package hf_8.list;
+
+import java.util.List;
+
+public class MyListTestDrive {
+
+    public static void main(String[] args) {
+        String[] ducks = { "Mallard Duck", "Redhead Duck", "Rubber Duck", "Decoy Duck" };
+        MyStringList ducksList = new MyStringList(ducks);
+
+        for (int i = 0; i < ducksList.size(); i++) {
+            System.out.println(ducksList.get(i));
+        }
+
+        String oldDuck = ducksList.set(3, "Donald Duck");
+        System.out.println("Replacing " + oldDuck);
+        System.out.println("New List: ");
+
+        for (int i = 0; i < ducksList.size(); i++) {
+            System.out.println(ducksList.get(i));
+        }
+
+        List<String> ducksSubList = ducksList.subList(2, 3);
+        System.out.println("Created a sub List of ducks, with size: " + ducksSubList.size());
+        for (int i = 0; i < ducksSubList.size(); i++) {
+            System.out.println(ducksSubList.get(i));
+        }
+    }
+}

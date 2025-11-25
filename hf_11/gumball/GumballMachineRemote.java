fbd636ba48fe72941103fcd1d3a317ceb4785433
Akanksha <akankshazc23@icloud.com>
2025-11-25 23:38:28 +0530

    Add GumballMachineRemote interface for remote access to gumball machine state and properties

---
+++ hf_11/gumball/GumballMachineRemote.java
@@ -0,0 +1,12 @@
+package hf_11.gumball;
+
+import java.rmi.RemoteException;
+import java.rmi.Remote;
+
+public interface GumballMachineRemote extends Remote {
+    public int getCount() throws RemoteException;
+
+    public String getLocation() throws RemoteException;
+
+    public State getState() throws RemoteException;
+}

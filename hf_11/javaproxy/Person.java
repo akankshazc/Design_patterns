ce6bc7350dbc8a58b07f7abee7469c8a691714f5
Akanksha <akankshazc23@icloud.com>
2025-12-01 21:36:24 +0530

    Add Person interface to define the structure for person-related data

---
+++ hf_11/javaproxy/Person.java
@@ -0,0 +1,19 @@
+package hf_11.javaproxy;
+
+public interface Person {
+    String getName();
+
+    String getGender();
+
+    String getInterests();
+
+    int getGeekRating();
+
+    void setName(String name);
+
+    void setGender(String gender);
+
+    void setInterests(String interests);
+
+    void setGeekRating(int rating);
+}
